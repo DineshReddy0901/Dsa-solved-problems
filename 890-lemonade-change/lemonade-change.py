41cc727f32d0d7c985b870ec9bc929744308b82e
DineshReddy0901 <dineshrakkireddy@gmail.com>
2026-02-09 12:45:45 +0530

    Time: 7 ms (40.85%) | Memory: 15.2 MB (70.55%) - LeetSync

---
+++ 890-lemonade-change/lemonade-change.py
@@ -0,0 +1,27 @@
+class Solution(object):
+    def lemonadeChange(self, bills):
+        count_5 =0
+        count_10 =0
+        
+        for i in bills:
+            if i==5:
+                count_5 +=1
+            elif i ==10:
+                if count_5==0:
+                    return False
+                else:
+                    count_5-=1
+                    count_10 += 1
+
+            else:
+                if count_10>0 and count_5>0:
+                    count_10-=1
+                    count_5-=1
+                elif count_5>=3:
+                    count_5-=3
+                else:
+                    return False
+            
+
+        return True
+        
