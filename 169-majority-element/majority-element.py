392275a17b9bca93e142abffdaf5aa43c30836ff
DineshReddy0901 <dineshrakkireddy@gmail.com>
2026-02-16 22:27:44 +0530

    Time: 21 ms (10.93%) | Memory: 13.8 MB (37.72%) - LeetSync

--- 169-majority-element/majority-element.py
+++ 169-majority-element/majority-element.py
@@ -1,7 +1,12 @@
 class Solution(object):
     def majorityElement(self, nums):
-        from collections import Counter
-        counts = Counter(nums)
-        return max(counts,key=counts.get)
-        
+        hash = {}
+        result = 0
+        highest =0
+        for i in nums:
+            hash[i] = 1+ hash.get(i,0)
+            if hash[i] > highest:
+                result = i
+                highest = hash[i]
+        return result
         
