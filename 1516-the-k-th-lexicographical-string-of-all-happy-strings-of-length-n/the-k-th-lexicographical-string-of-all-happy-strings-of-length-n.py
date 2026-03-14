2f4f8cd922dd05a908987e44a31e9a843a8dce2e
DineshReddy0901 <dineshrakkireddy@gmail.com>
2026-03-14 22:06:09 +0530

    Time: 9 ms (73.13%) | Memory: 12.3 MB (94.03%) - LeetSync

---
+++ 1516-the-k-th-lexicographical-string-of-all-happy-strings-of-length-n/the-k-th-lexicographical-string-of-all-happy-strings-of-length-n.py
@@ -0,0 +1,17 @@
+class Solution(object):
+    def getHappyString(self, n, k):
+        res = []
+        def dfs(s):
+            if len(res) >=k:
+                return
+            if len(s)==n:
+                res.append(s)
+                return
+            for c in "abc":
+                if not s or s[-1]!=c:
+                    dfs(s+c)
+        dfs("")
+        return res[k-1] if k<= len(res) else ""
+
+        
+        
