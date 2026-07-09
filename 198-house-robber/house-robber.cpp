7f238d165d67ba0e362cc3a7b86601e9c6e30687
DineshReddy0901 <dineshrakkireddy@gmail.com>
2026-07-09 21:54:05 +0530

    Time: 0 ms (100.00%) | Memory: 10.1 MB (78.71%) - LeetSync

--- 198-house-robber/house-robber.cpp
+++ 198-house-robber/house-robber.cpp
@@ -2,7 +2,7 @@ class Solution {
 public:
     int rob(vector<int>& nums) {
          int prev2 = 0; // dp[i-2]
-        int prev1 = 0; // dp[i-1]
+         int prev1 = 0; // dp[i-1]
 
         for (int money : nums) {
             int curr = max(prev1, prev2 + money);
