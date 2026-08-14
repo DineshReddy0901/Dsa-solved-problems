5f4c7cc9173011d8d96b72c0eb17082adc6caa91
DineshReddy0901 <dineshrakkireddy@gmail.com>
2026-08-14 07:24:59 +0530

    Time: 55 ms (58.78%) | Memory: 14.2 MB (76.58%) - LeetSync

--- 876-hand-of-straights/hand-of-straights.py
+++ 876-hand-of-straights/hand-of-straights.py
@@ -1,33 +1,17 @@
 class Solution(object):
     def isNStraightHand(self, hand, groupSize):
-        if len(hand)% groupSize!=0:
+        if len(hand)%groupSize != 0:
             return False
-        
-        count = Counter(hand)
-        min_heap = list(count.keys())
-        heapq.heapify(min_heap)
-
-        while min_heap:
-            start = min_heap[0]
-
-            for card in range(start,start+groupSize):
-                if count[card]==0:
+        freq = Counter(hand)
+        for i in sorted(hand):
+            if freq[i] == 0:
+                continue
+            for j in range(i,i+groupSize):
+                if freq[j]==0:
                     return False
-                count[card]-=1
-                # if count[card]==0:
-                #     if card!= min_heap[0]:
-                #         return False
-            while min_heap and count[min_heap[0]]==0:
-                heapq.heappop(min_heap)
+                freq[j]-=1
         return True
-                  
-
-            #         else:
-            #             heapq.heappop(min_heap)
-            # return True
 
 
-
-
-
+        
         
